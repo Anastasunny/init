8becc3f3ecc237e255c2bc25343ac775db9d048c
Anastasia <nastyamalets738@gmail.com>
2021-10-15 20:11:24 +0300

    ht9

---
+++ HT9.js
@@ -0,0 +1,23 @@
+const fs = require('fs-extra')
+
+function n(callback) {
+    callback();
+}
+
+n(() => {
+    fs.mkdir('./folder1', () => {
+        fs.createFile('./folder1/nfile.txt', () => {
+            fs.mkdir('./folder2', () => {
+                fs.move('./folder1/nfile.txt', './folder2/nfile.txt', () => {
+                    fs.remove('./folder2/nfile.txt', () => {
+                        fs.remove('./folder1', () => {
+                            fs.remove('./folder2', () => {
+                            console.log('7');
+                            })
+                        })        
+                    })
+                })
+            })
+        })
+    })
+})
